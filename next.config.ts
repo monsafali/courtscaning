6b2b659d04ef475abe4e3aae1d54a8c8f5e8d028
monsafali <monsafali272@gmail.com>
2026-06-17 11:16:30 +0500

    fix deployment error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
